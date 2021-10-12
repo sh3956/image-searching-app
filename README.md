4196d6632c1d271a3bf2eb9cfd0f2674fa523f46
sijia0815 <sijia@locus.co>
2021-10-11 22:23:59 -0400

    Create: framework for image searching engine

---
+++ README.md
@@ -0,0 +1,11 @@
+## Image Searching Engine Web App
+
+### About The Project
+
+### Built With
+
+### Getting Started
+
+### Oppurtunities
+
+### Contact
---
+++ feature_extractor.py
@@ -0,0 +1,29 @@
+import numpy as np
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.models import Model
+from tensorflow.keras.applications.vgg16 import VGG16, preprocess_input
+
+class FeatureExtractor:
+    """
+    Load image model from tensorflow and extract feature from an image
+    """
+    def __init__(self):
+        VGG_model = VGG16(weights='imagenet')
+        self.model = Model(inputs=VGG_model.input, outputs=base_model.get_layer('fc1').output)
+    
+    def extract(self, img):
+        """
+        Extract feature from image
+        params:
+            img: from PIL.Image.open(path) or tensorflow.keras.preprocessing.image.load_img(path)
+        """
+        img = img.resize((224, 224))
+        img = img.convert_to("RGB")
+
+        x = image.img_to_array(img)
+        x = np.expand_dims(x, axis=0)
+        x = preprocess_input(x)
+
+        feature = self.model.predict(x)[0]
+        return feature / np.linalg.norm(feature)
+
