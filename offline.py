e3c3c60371e9d1fccc6449f37403f9fb229b521f
sijia0815 <sijia@locus.co>
2021-10-11 22:28:30 -0400

    Created: offline calculation function

---
+++ offline.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+
+import numpy as np
+from PIL import Image
+
+from feature_extractor import FeatureExtractor
+
+
+if __name__ == '__main__':
+    fe = FeatureExtractor()
+
+    for img_path in sorted(Path("./static/img").glob("*.jpg")):
+        feature = fe.extract(img=Image.open(img_path))
+        feature_path = Path("./static/feature") / (img_path.stem + ".npy") 
+        np.save(feature_path, feature)
