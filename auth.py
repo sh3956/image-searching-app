ffd50ed0fb27afb84bfea0de638fc0c474a3540f
sijia0815 <sh3956@columbia.edu>
2021-10-18 00:38:56 -0400

    Fix: reorganize order of importing packages

--- auth.py
+++ auth.py
@@ -1,8 +1,10 @@
 from flask import Blueprint, render_template, redirect, url_for, request, flash
-from werkzeug.security import generate_password_hash, check_password_hash
-from models import User
 from flask_login import login_user, logout_user, login_required, current_user
+
+from werkzeug.security import generate_password_hash, check_password_hash
+
 from __init__ import db
+from models import User
 
 
 auth = Blueprint('auth', __name__) 
--- src/features/offline.py
+++ src/features/offline.py
@@ -1,3 +1,4 @@
+import ssl
 from pathlib import Path
 
 import numpy as np
@@ -5,8 +6,8 @@ from PIL import Image
 
 from feature_extractor import FeatureExtractor
 
-import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
+
 
 if __name__ == '__main__':
     fe = FeatureExtractor()
